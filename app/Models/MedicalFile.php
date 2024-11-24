27d2fa4ab0258a90de0d27cabc495314851520bf
Ali-S-Mohamad <victory.asm.2000@gmail.com>
2024-11-24 20:51:33 +0200

    adding relationships

--- app/Models/MedicalFile.php
+++ app/Models/MedicalFile.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class MedicalFile extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+
+
+    public function patient(){
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function prescriptions(){
+        return $this->hasMany(Prescription::class);
+    }
 }
--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Patient extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function medicalFiles(){
+        return $this->hasMany(MedicalFile::class);
+    }
+
+    public function appointments(){
+        return $this->hasMany(Appointment::class);
+    }
 }
