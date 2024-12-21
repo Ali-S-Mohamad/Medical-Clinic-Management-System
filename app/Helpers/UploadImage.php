c5bed7d57e661069feea14bd7c384d5d2e3559a9
Alaa NAjeeb <alaanajeeb91@gmail.com>
2024-12-21 23:47:16 +0200

    employee image, rating blade

--- app/Helpers/UploadImage.php
+++ app/Helpers/UploadImage.php
@@ -24,28 +24,4 @@ if (!function_exists('saveImage')) {
         }
 }
 
-    // function updateImage($folder, $request, $employee, $disk='public')
-    // {
-    //     if ($request->hasFile('profile_image')) {
-    //         $existingImage = $employee->image;
-
-    //         // delete image from disk + delete path from image table
-    //         if ($existingImage) {
-    //             Storage::disk($disk)->delete($existingImage->image_path);
-    //             $existingImage->delete();
-    //         }
-
-    //         // upload image
-    //         $path = $request->file('profile_image');
-    //         $image_name = time() . '_' . $path->getClientOriginalName(); 
-    //         $path = $path->storeAs($folder, $image_name, $disk);
-
-    //         // save image path in images table
-    //         $image = new Image();
-    //         $image->image_path = $path;
-    //         $image->imageable_id = $employee->id; // ID الموظف
-    //         $image->imageable_type = 'App\Models\Employee';
-    //         $image->save();
-    //     }
-    // }
 }
